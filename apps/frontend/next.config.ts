10b61f189381752d839c22bf7ed1926047023eb3
glucrypto <glucrypto@protonmail.com>
2024-11-15 06:23:47 -0500

    fix: eslint rule

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  eslint: {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
